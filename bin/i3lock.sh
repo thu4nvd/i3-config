3bc4eee606bfed8a3672f7dac5ca34a41ad526e1
Duc Thuan VU D <dtvu@linagora.com>
2017-04-20 23:12:02 +0700

    tuning i3lock and little conky

--- bin/i3lock.sh
+++ bin/i3lock.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-scrot /tmp/screen_locked.png
+#scrot /tmp/screen_locked.png
 #convert /tmp/screen_locked.png -blur 2x2  -charcoal 1 -colorize 70%   /tmp/screen_locked.png
 #convert /tmp/screen_locked.png -blur 3x3  -implode 2 -colorize 70%   /tmp/screen_locked.png
 #convert /tmp/screen_locked.png -pointsize 50 -fill white -draw 'text 300,200 "TxGVNN"' /tmp/screen_locked.png
-convert /tmp/screen_locked.png -scale 20% -scale 500% /tmp/screen_locked.png
-convert /tmp/screen_locked.png ~/.i3/bin/lock.png -gravity center -composite -matte /tmp/screen_locked.png
-i3lock -u -i /tmp/screen_locked.png
+#convert /tmp/screen_locked.png -scale 20% -scale 500% /tmp/screen_locked.png
+#convert /tmp/screen_locked.png ~/.i3/bin/lock.png -gravity center -composite -matte /tmp/screen_locked.png
+#i3lock -u -i /tmp/screen_locked.png
+i3lock -i $(find ~/.i3/bin/lock-images | shuf -n1)
